743174fe3ab2ed789950fdbf63630b86308b2cf1
John <john.haytko@xero.com>
2018-12-10 16:57:13 -0800

    Take advantage of graphQLResultHasError

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,11 @@
-import { getMainDefinition } from 'apollo-utilities';
+import {
+  getMainDefinition,
+  graphQLResultHasError
+} from 'apollo-utilities';
 
 export const isQuery = operation => operation === 'query';
 export const isMutation = operation => operation === 'mutation';
-export const isSuccessful = result => result.data && !result.data.error && !result.errors;
+export const isSuccessful = result => !graphQLResultHasError(result);
 export const isSuccessfulQuery = (operation, result) => isQuery(operation) && isSuccessful(result);
 export const isSuccessfulMutation = (operation, result) => isMutation(operation) && isSuccessful(result);
 export const isFailedMutation = (operation, result) => isMutation(operation) && !isSuccessful(result);
